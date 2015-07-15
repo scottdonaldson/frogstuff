a06f2fcdb8022b0e06ec72ae9c12771338a78421
lisaotto <otto.elizabeth@gmail.com>
2015-07-15 17:45:59 -0400

    updated graphics

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<div>
 		<img class="background-img" src="img/Lisa&Scott-17.jpg">
 		<div class="content toptext halfcolumn">
+			<img class="cursive fullwidth" alt="The Celebration" src="img/celebration.png">
 			<h3>Join us for Dinner, Dancing &amp; Vows of Everlasting Commitment</h3>
 			<p>Saturday, May 21, 2016 <br>
 				at five in the evening</p>
@@ -23,7 +24,7 @@
 
 	<div class="content">
 		<div class="clearfix halfcolumn">
-			<h2>Where to stay</h2>
+			<img class="cursive" alt="Where to Stay" src="img/wheretostay.png">
 			<p>A block of rooms have been booked at newly renovated Morrison Clark Hotel. <br>
 			<p>It's a 6 minute walk to reception and a 7 minute walk to metro.</p>
 			<p><a href="https://www.google.com/maps/d/edit?mid=zRNfw6tt-0CE.k0nw9bIx8P_4&usp=sharing" target="_blank">1015 L St NW, Washington DC 20001 </a><br>
@@ -33,31 +34,33 @@
 		<img class="halfcolumn clearfix" src="img/wreath.png" alt="">
 	</div>
 
-	<img class="divider" src="img/divider.png" alt="">
+	<img class="divider" src="img/divider2.png" alt="">
 
 	<div class="content clearfix">
 		
-		<div class="clearfix right halfcolumn">
-			<h2> What to do in DC</h2>
+		<div class="clearfix halfcolumn">
+			<img class="cursive" alt="What to Do in DC" src="img/whattodo.png">
 			<h3> Touristy Hidden Gems</h3>
 			<p> Ready to pack up your fanny pack and try out some off-the-beaten path museums? Start off the day by heading over to the Colombe a hidden coffee shop in the alley behind the reception gallery. Then walk on over to either the Building Museum or the Postal Museum (or both if you’re feeling frisky!). Once you’re museumed out, drop into either Graffiato’s (funky small plates) or Busboys &amp; Poets (DC institution sandwich shop) for a scrumptious lunch. </p>
 			<a href="https://www.google.com/maps/d/edit?mid=zRNfw6tt-0CE.kzVJlxw6FzSg&usp=sharing" target="_blank">Let's get site-seeing!</a>
 		</div>
 		<img class="halfcolumn clearfix" src="img/wreath.png" alt="">
-		<img class="halfcolumn right clearfix" src="img/wreath.png" alt="">
-		<div class="halfcolumn clearfix">
+
+		<div class="halfcolumn right clearfix">
 			<h3>A Local's Walking Tour</h3>
 			<p>Interested in a local’s stroll through DC? Head over to the MT Vernon Metro stop, buy yourself a metro card and take the the green line up to Columbia Heights (no transfers necessary!). Stop by Sticky Fingers (we literally lived three windows above) for a vegan muffin (you won’t even know! we promise!). After wandering through the Park, stop by Pho 14 for lunch and try the veggie Pho (It’s made with apples!). If you’ve still got gas in the engine, you can walk down to Dupont circle where you can check out Books-A-Million or Kramer Books and people watch. See, it’s not all brutalist office building after all! </p>
 			<a href="https://www.google.com/maps/d/edit?mid=zRNfw6tt-0CE.kzVJlxw6FzSg&usp=sharing" target="_blank">I'm ready to mosey!</a>
 		</div>
+		<img class="halfcolumn clearfix" src="img/wreath.png" alt="">
 	</div>
 
 	<img class="divider" src="img/divider.png" alt="">
 
 
 	<div class="content clearfix">
-		<div class="clearfix"> 
-			<h2>Gifts</h2>
+		<div class="clearfix halfcolumn"> 
+
+			<img class="cursive" alt="Gifting" src="img/gifting.png">
 			<p>In the past 6 years we've lived in seven apartments and not sure we're we'll be moving to in another year. So we request that you don't bring us gifts. We're fortunate enough to have you in our lives (and hopefully at the celebration). Instead of gifts, we'd love to have you make a donation. </p>
 
 			<p>Donate where you feel like it will do the most good but here are two organizations we're vetted for you:</p>
@@ -69,8 +72,9 @@
 
 	<div>
 		<img class="background-img" src="img/Lisa&Scott-64.jpg">
-		<div class="content toptext halfcolumn">
-			<h3>Hope to See You There!!</h3>
+
+		<div class="content toptext twocolumn">
+			<img class="cursive fullwidth" alt="See You there!" src="img/seeyouthere.png">
 			<h5>&copy; Lisa &amp; Scott 2006-divorce </h5>
 			<h5>All photos by <a href="http://uniquelapin.com" target="_blank">Unique Lapin</a>.</h5>
 		</div>
