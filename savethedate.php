6cb7579e5d064468938d22ec54a5cd1430feb1c9
Scott Donaldson <scott.p.donaldson@gmail.com>
2015-08-19 19:20:45 -0400

    no-JS fallback text update

--- savethedate.php
+++ savethedate.php
@@ -55,7 +55,7 @@ include('includes/header.php'); ?>
 		<div class="gradient-bg"></div>
 		<img src="<?= $url; ?>/img/savethedate.png">
 		<p>This website uses JavaScript so that you can send us your address. But it looks like you don't have JavaScript enabled.</p>
-		<p>That's OK!</p>
+		<p>It's a little weird, but that's OK! (It's not like it's what we do for a living or anything...)</p>
 		<p>You can still send us your address over email at <a href="mailto:scott.p.donaldson@gmail.com">scott.p.donaldson@gmail.com</a>.</p>
 		<br>
 		<h5>All photos by <a href="http://uniquelapin.com" target="_blank">Unique Lapin</a>.</h5>
